d199c2857cb09ab83cd317aab680d4551b7e0dc2
Patrik Vilja <patrik.vilja@gmail.com>
2015-02-09 22:02:16 +0200

    add first virtual-dom test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,15 +8,28 @@ R.installTo(window);
 import Future from 'data.future';
 import Maybe from 'data.maybe';
 import {EventStream, Behavior} from 'reactive';
+import {diff, patch, h, create} from 'virtual-dom';
 
 var log = x => { console.log(x); return x; };
+
+let render = amount => {
+  return h('div', String(amount));
+};
+
+let tree = render(0);
+let rootNode = create(tree);
+document.body.appendChild(rootNode);
+
 var fetch = path => EventStream.fromEventSource(new EventSource(path));
 var tweets = fetch('/api/tweets').map(compose(JSON.parse, get('data')));
 
 tweets
   .scan((a, b) => a + 1, 0)
   .subscribe((amount) => {
-    console.log('amount', amount);
+    var newTree = render(amount);
+    var patches = diff(tree, newTree);
+    rootNode = patch(rootNode, patches);
+    tree = newTree;
   });
 
 var withGeo = tweets.filter(compose(is(Object), get('geo')));
