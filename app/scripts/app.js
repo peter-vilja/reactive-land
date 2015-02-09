31593850bb6e18a90c30ca3aa65460cdc1c0691d
Peter Vilja <peter.vilja@gmail.com>
2015-02-09 21:03:30 +0200

    make a stream for tweets which have geo hash

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,24 @@
 'use strict';
 import pf from 'pointfree-fantasy';
-pf.expose(window);
+// pf.expose(window);
+
+import R from 'ramda';
+R.installTo(window);
 
 import Future from 'data.future';
 import Maybe from 'data.maybe';
-import _ from 'ramda';
 import {EventStream, Behavior} from 'reactive';
 
-var fetch = (path) => EventStream.fromEventSource(new EventSource(path));
-var tweets = fetch('/api/tweets');
+var log = x => { console.log(x); return x; };
+var fetch = path => EventStream.fromEventSource(new EventSource(path));
+var tweets = fetch('/api/tweets').map(compose(JSON.parse, get('data')));
 
 tweets
   .scan((a, b) => a + 1, 0)
   .subscribe((amount) => {
     console.log('amount', amount);
   });
+
+tweets
+  .filter(compose(is(Object), get('geo')))
+  .subscribe(log);
