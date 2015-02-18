285176159f659b9aa5b29973c0bd1d386f5b1051
Peter Vilja <peter.vilja@gmail.com>
2015-02-18 10:36:17 +0200

    render hashtags

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,12 +12,13 @@ import {diff, patch, h, create} from 'virtual-dom';
 
 var log = x => { console.log(x); return x; };
 var select = x => document.querySelector(x);
+var unshift = R.curry((selector, dom) => select(selector).insertBefore(dom, select(selector).firstChild));
 
 let counter = amount => h('div', {className: 'tweet-count'}, String(amount));
 
 let initial = counter(0);
 let rootNode = create(initial);
-select('.metrics').insertBefore(rootNode, select('.metrics').firstChild);
+unshift('.metrics', rootNode);
 
 let render = Behavior.of(initial);
 
@@ -30,12 +31,35 @@ var tweets = fetch('/api/tweets').map(compose(JSON.parse, get('data')));
 
 tweets
   .scan((a, b) => a + 1, 0)
-  .subscribe((amount) => {
-    render.next(counter(amount))
-  });
+  .subscribe(R.compose(render.next.bind(render), counter));
 
 var withCoordinates = tweets.filter(compose(is(Object), get('coordinates')));
 var WithTag = tweets
   .filter(get('entities'))
   .map(compose(map(compose(toLower, get('text'))), get('hashtags'), get('entities')))
   .filter(compose(lt(-1), indexOf('love')));
+
+let keywords = fetch('/api/keywords').map(compose(JSON.parse, get('data')));
+
+let feeling = () => {
+  let colors = ['#f05032', '#f79727', '#fec325', '#54b847', '#b319ab', '#692c90', '#00539e', '#0078c1'];
+  let rand = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+  let li = text => h('li', {
+    style: {
+      top: rand(0, 100) + '%',
+      left: rand(0, 100) + '%',
+      fontSize: rand(10, 30) + 'px',
+      color: colors[rand(0, 7)]
+    }
+  }, text);
+  let renderFeeling = R.compose(unshift('.hashtags'), create, li);
+
+  let filterByKeyword = R.curry((stream, keyword) => stream.filter(R.compose(lt(-1), strIndexOf(keyword), toLower, get('text'))));
+  let filter = filterByKeyword(keywords);
+  let track = feelings => feelings.forEach(feeling => filter(feeling).subscribe(x => renderFeeling(feeling)));
+
+  track(['happy', 'joyful', 'excited',
+    'relaxed', 'awesome', 'sad', 'depressed', 'bored', 'angry', 'hurt']);
+};
+
+feeling();
