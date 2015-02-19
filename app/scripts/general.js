ab386c46da90e2ad53ec2e4f47d773ce95deb0f2
Peter Vilja <peter.vilja@gmail.com>
2015-02-19 12:18:50 +0200

    show feelings with circles

--- app/scripts/general.js
+++ app/scripts/general.js
@@ -5,3 +5,4 @@ export const fetch = path => EventStream.fromEventSource(new EventSource(path));
 export const log = x => { console.log(x); return x; };
 export const select = x => document.querySelector(x);
 export const unshift = curry((selector, dom) => select(selector).insertBefore(dom, select(selector).firstChild));
+export const rand = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
--- app/scripts/keywords.js
+++ app/scripts/keywords.js
@@ -1,45 +1,34 @@
 'use strict';
 import {EventStream, Behavior} from 'reactive';
 import {diff, patch, h, create} from 'virtual-dom';
-import {fetch, log, select, unshift} from './general';
+import {fetch, log, select, unshift, rand} from './general';
 
-let keywords = fetch('/api/keywords').map(compose(JSON.parse, get('data')));
-let colors = ['#8f9c7f', '#f07429', '#f1d78d', '#cd9fc3', '#cccbad', '#e15524', '#699eb8'];
-let rand = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
-let li = text => h('li', {
-  style: {
-    top: rand(0, 100) + '%',
-    left: rand(0, 100) + '%',
-    fontSize: rand(10, 30) + 'px',
-    color: colors[rand(0, 7)]
-  }
-}, text);
-let lis = [];
+let words = ['happy', 'joyful', 'excited', 'relaxed', 'awesome', 'sad', 'depressed', 'bored', 'angry', 'hurt'];
 
-let ul = li => h('ul', {className: 'hashtags'}, li);
-let tree = ul(lis);
-let tags = create(tree);
-unshift('.feeling', tags);
-
-let add = item => {
-  var list = take(29, lis);
-  list.unshift(item);
-  lis = list;
-  return list;
-};
+let createFeeling = (text, animate) => h('li', {className: animate ? text + ' animate' : text}, text);
+let createList = list => h('ul', {className: 'hashtags'}, list);
 
 let update = updated => {
-  var newTree = ul(updated);
+  var newTree = createList(updated);
   var patches = diff(tree, newTree);
   patch(tags, patches);
   tree = newTree;
 };
 
-let renderFeeling = compose(update, add, li);
+let render = feeling => words.map(word => createFeeling(word, word === feeling));
 
-let filterByKeyword = curry((stream, keyword) => stream.filter(get('text')).filter(compose(lt(-1), strIndexOf(keyword), toLower, get('text'))));
+let tree = createList(render());
+let tags = create(tree);
+unshift('.feeling', tags);
+
+let renderFeeling = compose(update, render);
+
+let filterByKeyword = curry((stream, keyword) => stream
+  .filter(get('text'))
+  .filter(compose(lt(-1), strIndexOf(keyword), toLower, get('text'))));
+
+let keywords = fetch('/api/keywords').map(compose(JSON.parse, get('data')));
 let filter = filterByKeyword(keywords);
 let track = feelings => feelings.forEach(feeling => filter(feeling).subscribe(x => renderFeeling(feeling)));
 
-track(['happy', 'joyful', 'excited',
-  'relaxed', 'awesome', 'sad', 'depressed', 'bored', 'angry', 'hurt']);
+track(words);
